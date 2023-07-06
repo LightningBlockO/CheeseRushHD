ca01bfb12bf47dafb013b8ee145b4ba9b37e865d
BigBoi12348 <107095940+BigBoi12348@users.noreply.github.com>
2023-07-06 15:02:11 +1000

    Fork and Knife

--- Cheese Rush HD/Assets/Scripts/Chris_Naughty_Corner/Enemy_Scripts/Enemy_Damage.cs
+++ Cheese Rush HD/Assets/Scripts/Chris_Naughty_Corner/Enemy_Scripts/Enemy_Damage.cs
@@ -7,7 +7,7 @@ public class Enemy_Damage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
