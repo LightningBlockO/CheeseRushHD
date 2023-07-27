631e236f07718967c89ae8b0fe18b2632330fb9d
BigBoi12348 <107095940+BigBoi12348@users.noreply.github.com>
2023-07-27 20:37:18 +1000

    Pause Menu

---
+++ Cheese Rush HD/Assets/Scripts/Chris_Naughty_Corner/Menu_Script/Pause_Menu.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Pause_Menu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public  Player_Movement rat;
+    void Start()
+    {
+        rat.GetComponent<Player_Movement>();
+        UnlockMouse();
+        pauseMenu.SetActive(false);
+
+    }
+    void UnlockMouse()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.V))
+        {
+            rat.GetComponent<Player_Movement>().enabled = false;
+            pauseMenu.SetActive(true);
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            pauseMenu.SetActive(false);
+            rat.GetComponent<Player_Movement>().enabled = true;
+        }
+        if (Input.GetKey(KeyCode.M))
+        {
+            SceneManager.LoadScene("Menu Scene");
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            Application.Quit();
+        }
+
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Cursor.visible = false;
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu Scene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
