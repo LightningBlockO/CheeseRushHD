720b157762cf66c0b67acb1542620a529710cc4d
LightningBlockO <domara.oraham@gmail.com>
2023-08-17 16:24:40 +1000

    Many Gameplay Fixes

--- Cheese Rush HD/Assets/Scripts/Timer.cs
+++ Cheese Rush HD/Assets/Scripts/Timer.cs
@@ -56,7 +56,7 @@ public class Timer : MonoBehaviour
     }
     IEnumerator AfterTime()
     {
-        yield return new WaitForSeconds(120f);
+        yield return new WaitForSeconds(165f);
         cheeseFaceEarly.GetComponent<MeshRenderer>().enabled = true;
         cheeseFaceEarlyCollider.GetComponent<Collider>().enabled = true;
     }
