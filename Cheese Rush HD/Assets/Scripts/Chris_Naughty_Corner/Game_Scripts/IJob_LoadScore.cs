5d6b2d30fab3b52fac5e9df597b43308ab1a600f
BigBoi12348 <107095940+BigBoi12348@users.noreply.github.com>
2023-08-23 18:20:38 +1000

    tidy ijob loadscene

--- Cheese Rush HD/Assets/Scripts/Chris_Naughty_Corner/Game_Scripts/IJob_LoadScore.cs
+++ Cheese Rush HD/Assets/Scripts/Chris_Naughty_Corner/Game_Scripts/IJob_LoadScore.cs
@@ -12,15 +12,11 @@ public class IJob_LoadScore : MonoBehaviour
     {
         public void Execute()
         {
-            // Load the high score system during initialization
             LoadHighScoreSystem();
-
-            // Other initialization tasks can also be added here
         }
 
         private void LoadHighScoreSystem()
         {
-            // Use the LeaderBoard script logic here to load the high scores
             LeaderBoard leaderBoard = new LeaderBoard();
             leaderBoard.GetLeaderboard();
         }
@@ -33,15 +29,9 @@ public class IJob_LoadScore : MonoBehaviour
 
     private IEnumerator InitializeGameWithJob()
     {
-        // Create the InitializationJob instance
         InitializationJob initializationJob = new InitializationJob();
-
-        // Schedule the job
         JobHandle initializationJobHandle = initializationJob.Schedule();
-
-        // Wait for the job to complete
         yield return new WaitWhile(() => !initializationJobHandle.IsCompleted);
-
         Debug.Log("Game initialization complete.");
     }
 }
