2b9931c3952bf373eda679c802780c8a7f3820c5
BigBoi12348 <107095940+BigBoi12348@users.noreply.github.com>
2023-08-25 12:13:23 +1000

    ficong suff

--- Cheese Rush HD/Assets/Scripts/Chris_Naughty_Corner/Game_Scripts/IJob_Image.cs
+++ Cheese Rush HD/Assets/Scripts/Chris_Naughty_Corner/Game_Scripts/IJob_Image.cs
@@ -1,54 +1,54 @@
-using System.Collections.Generic;
-using UnityEngine;
-using Unity.Collections;
-using Unity.Jobs;
-using Unity.Burst;
-public class IJob_Image : MonoBehaviour
-{
-    public string[] texturePaths;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Unity.Collections;
+//using Unity.Jobs;
+//using Unity.Burst;
+//public class IJob_Image : MonoBehaviour
+//{
+//    public string[] texturePaths;
 
-    private void Start()
-    {
-        LoadTextures();
-    }
+//    private void Start()
+//    {
+//        LoadTextures();
+//    }
 
-    private void LoadTextures()
-    {
-        List<LoadTextureJob> jobs = new List<LoadTextureJob>();
-        NativeArray<JobHandle> jobHandles = new NativeArray<JobHandle>(texturePaths.Length, Allocator.Temp);
+//    private void LoadTextures()
+//    {
+//        List<LoadTextureJob> jobs = new List<LoadTextureJob>();
+//        NativeArray<JobHandle> jobHandles = new NativeArray<JobHandle>(texturePaths.Length, Allocator.Temp);
 
-        for (int i = 0; i < texturePaths.Length; i++)
-        {
-            LoadTextureJob job = new LoadTextureJob
-            {
-                texturePath = texturePaths[i]
-            };
-            jobs.Add(job);
+//        for (int i = 0; i < texturePaths.Length; i++)
+//        {
+//            LoadTextureJob job = new LoadTextureJob
+//            {
+//                texturePath = texturePaths[i]
+//            };
+//            jobs.Add(job);
 
-            jobHandles[i] = job.Schedule();
-        }
+//            jobHandles[i] = job.Schedule();
+//        }
 
-        JobHandle.CompleteAll(jobHandles);
-        jobHandles.Dispose();
-    }
+//        JobHandle.CompleteAll(jobHandles);
+//        jobHandles.Dispose();
+//    }
 
-    [BurstCompile]
-    struct LoadTextureJob : IJob
-    {
-        public string texturePath;
+//    [BurstCompile]
+//    struct LoadTextureJob : IJob
+//    {
+//        public string texturePath;
 
-        public void Execute()
-        {
-            Texture2D loadedTexture = LoadTextureFromFile(texturePath);
-            Debug.Log("Loaded texture: " + texturePath);
-        }
+//        public void Execute()
+//        {
+//            Texture2D loadedTexture = LoadTextureFromFile(texturePath);
+//            Debug.Log("Loaded texture: " + texturePath);
+//        }
 
-        private Texture2D LoadTextureFromFile(string path)
-        {
-            byte[] bytes = System.IO.File.ReadAllBytes(path);
-            Texture2D texture = new Texture2D(2, 2);
-            texture.LoadImage(bytes);
-            return texture;
-        }
-    }
-}
+//        private Texture2D LoadTextureFromFile(string path)
+//        {
+//            byte[] bytes = System.IO.File.ReadAllBytes(path);
+//            Texture2D texture = new Texture2D(2, 2);
+//            texture.LoadImage(bytes);
+//            return texture;
+//        }
+//    }
+//}
--- Cheese Rush HD/Assets/Scripts/Chris_Naughty_Corner/Game_Scripts/IJob_LoadScore.cs
+++ Cheese Rush HD/Assets/Scripts/Chris_Naughty_Corner/Game_Scripts/IJob_LoadScore.cs
@@ -1,37 +1,37 @@
-using System.Collections;
-using UnityEngine;
-using Unity.Jobs;
-using Unity.Collections;
-using Unity.Burst;
-using Dan.Main;
+//using System.Collections;
+//using UnityEngine;
+//using Unity.Jobs;
+//using Unity.Collections;
+//using Unity.Burst;
+//using Dan.Main;
 
-public class IJob_LoadScore : MonoBehaviour
-{
-    [BurstCompile]
-    struct InitializationJob : IJob
-    {
-        public void Execute()
-        {
-            LoadHighScoreSystem();
-        }
+//public class IJob_LoadScore : MonoBehaviour
+//{
+//    [BurstCompile]
+//    struct InitializationJob : IJob
+//    {
+//        public void Execute()
+//        {
+//            LoadHighScoreSystem();
+//        }
 
-        private void LoadHighScoreSystem()
-        {
-            LeaderBoard leaderBoard = new LeaderBoard();
-            leaderBoard.GetLeaderboard();
-        }
-    }
+//        private void LoadHighScoreSystem()
+//        {
+//            LeaderBoard leaderBoard = new LeaderBoard();
+//            leaderBoard.GetLeaderboard();
+//        }
+//    }
 
-    private void Start()
-    {
-        StartCoroutine(InitializeGameWithJob());
-    }
+//    private void Start()
+//    {
+//        StartCoroutine(InitializeGameWithJob());
+//    }
 
-    private IEnumerator InitializeGameWithJob()
-    {
-        InitializationJob initializationJob = new InitializationJob();
-        JobHandle initializationJobHandle = initializationJob.Schedule();
-        yield return new WaitWhile(() => !initializationJobHandle.IsCompleted);
-        Debug.Log("Game initialization complete.");
-    }
-}
+//    private IEnumerator InitializeGameWithJob()
+//    {
+//        InitializationJob initializationJob = new InitializationJob();
+//        JobHandle initializationJobHandle = initializationJob.Schedule();
+//        yield return new WaitWhile(() => !initializationJobHandle.IsCompleted);
+//        Debug.Log("Game initialization complete.");
+//    }
+//}
