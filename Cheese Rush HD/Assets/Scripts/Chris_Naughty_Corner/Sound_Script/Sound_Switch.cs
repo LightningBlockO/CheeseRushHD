b95245352f4bd2f3aefefa72e0edc1066ff00e1e
BigBoi12348 <107095940+BigBoi12348@users.noreply.github.com>
2023-12-04 19:37:14 +1100

    Level 2 has been addede music swithc and skybox

---
+++ Cheese Rush HD/Assets/Scripts/Chris_Naughty_Corner/Sound_Script/Sound_Switch.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_Switch : MonoBehaviour
+{
+    public GameObject audioLevel1;
+    public GameObject audioLevel2;
+    public Material skyboxLevel1;
+    public Material skyboxLevel2;
+
+    void Awake()
+    {
+        audioLevel2.SetActive(false);
+        RenderSettings.skybox = skyboxLevel1;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("Rat"))
+        {
+            audioLevel1.SetActive(false);
+            audioLevel2.SetActive(true);
+            RenderSettings.skybox = skyboxLevel2;
+        }
+    }
+}
